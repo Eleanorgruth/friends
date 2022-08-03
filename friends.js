e0ab4138b25943418b94f629e10901bff1220a44
Eleanor Ruth <ellie.ruth@me.com>
2022-08-03 16:51:33 -0600

    Add elsif statement

--- friends.js
+++ friends.js
@@ -1,8 +1,12 @@
-var bestFriend = "Eleanor"
+var bestFriend = "Nick"
+// var friend = "Nick"
 
 if (bestFriend === "Albert") {
   console.log(`Hi ${bestFriend}`)
 }
+  else if(bestFriend === "Nick"){
+    console.log (`What's Up ${bestFriend}`)
+  }
   else {
     console.log(`Hello`)
   }
